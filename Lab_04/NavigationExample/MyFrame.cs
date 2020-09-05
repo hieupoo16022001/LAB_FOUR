96154cc27a81056f155bb34921594711c2d29fab
hieupoo16022001 <54589137+hieupoo16022001@users.noreply.github.com>
2020-09-05 12:02:29 +0700

    lab4

---
+++ Lab_04/NavigationExample/MyFrame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NavigationExample
+{
+    internal class MyFrame
+    {
+        public static bool CanGoBack { get; internal set; }
+        public static bool CanGoForward { get; internal set; }
+
+        internal static void Navigate(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void GoBack()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void GoForward()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
